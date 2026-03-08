4af7a33f4708e9efa8569a0e64e1f29b9f6721f7
H-N-S-Abayawardhana <hnsabayawardhana@gmail.com>
2026-03-08 10:39:56 +0530

    feat: update the ui in skin disease component

--- src/components/dashboard/Sidebar.tsx
+++ src/components/dashboard/Sidebar.tsx
@@ -127,6 +127,12 @@ const SIDEBAR_NAV_ITEMS: SidebarNavItem[] = [
     roles: ["SUPER_ADMIN", "VETERINARIAN", "USER"],
   },
   {
+    name: "Upload Prescription",
+    href: "/dashboard/upload-prescriptions",
+    icon: DocumentTextIcon,
+    roles: ["SUPER_ADMIN", "VETERINARIAN", "USER", "PHARMACIST"],
+  },
+  {
     name: "Pharmacy",
     icon: BuildingStorefrontIcon,
     roles: ["SUPER_ADMIN", "VETERINARIAN", "USER", "PHARMACIST"],
